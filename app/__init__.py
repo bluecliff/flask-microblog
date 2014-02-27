97b9ab5ab258e99653b115c969acbf64e4cc25f3
lishuangjiang <li0shuangjiang@gmail.com>
2014-02-27 16:50:24 +0800

    增加数据模型，使用了flask-migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
 from flask.ext.script import Manager
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.migrate import Migrate
 
 app = Flask(__name__)
-manager = Manager(app)
-app.config.from_object('config')
 
-from app import views
+app.config.from_object('config')
+db = SQLAlchemy()
+db.app = app
+db.init_app(app)
+
+migrate = Migrate(app,db)
+manager = Manager(app)
+#import sys
+#print sys.path
+from app import views,models
--- manage.py
+++ manage.py
@@ -1,4 +1,6 @@
 from app import app, manager
+from flask.ext.migrate import MigrateCommand
 
+manager.add_command('db',MigrateCommand)
 app.debug=True
 manager.run()
