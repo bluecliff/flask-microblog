3b8301cf1268ffdf2f7932c7aabdd3b610d32925
lishuangjiang <li0shuangjiang@gmail.com>
2014-02-27 21:22:03 +0800

    增加openid认证登录等处过程，基于flask-login和flask-openid

--- app/__init__.py
+++ app/__init__.py
@@ -2,16 +2,27 @@ from flask import Flask
 from flask.ext.script import Manager
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.migrate import Migrate
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
 
 app = Flask(__name__)
 
 app.config.from_object('config')
+
 db = SQLAlchemy()
 db.app = app
 db.init_app(app)
 
 migrate = Migrate(app,db)
 manager = Manager(app)
+
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view='login'
+oid = OpenID(app,os.path.join(basedir,'tmp'))
+
 #import sys
 #print sys.path
 from app import views,models
--- app/models.py
+++ app/models.py
@@ -9,8 +9,21 @@ class User(db.Model):
     email = db.Column(db.String(120),index=True,unique=True)
     role = db.Column(db.SmallInteger,default=ROLE_USER)
     posts = db.relationship('Post',backref='author',lazy='dynamic')
+    
     def __repr__(self):
         return '<User %r>' %(self.nickname)
+
+    def is_authenticated(self):
+        return True
+    
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
 
 class Post(db.Model):
     id = db.Column(db.Integer,primary_key=True)
